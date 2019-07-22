a0588ab3395e13917ef35dcf7273255b4d037ac6
Mrshieh817 <18716398693@163.com>
2019-07-22 14:40:02 +0800

    提交一波

--- springcloud-provider/src/main/java/com/igxe/Controller/errorController.java
+++ springcloud-provider/src/main/java/com/igxe/Controller/errorController.java
@@ -2,13 +2,14 @@ package com.igxe.Controller;
 
 import java.util.HashMap;
 import java.util.Map;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.UnsatisfiedServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -21,11 +22,42 @@ import lombok.extern.slf4j.Slf4j;
 public class errorController {
 	@ExceptionHandler(Throwable.class)
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-	public Object exception(Throwable throwable, HttpServletResponse response) {
+	public Object exception(Throwable throwable) {
 		log.error("系统异常", throwable);
-		Map<String, Object> map = new HashMap<>();	
+		Map<String, Object> map = new HashMap<>();
 		map.put("mesage", throwable.getMessage());
-		map.put("code", response.getStatus());
+		map.put("code", HttpStatus.INTERNAL_SERVER_ERROR.value());
+		return map;
+
+	}
+
+	@ExceptionHandler({ IllegalArgumentException.class })
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public Object badRequestException(IllegalArgumentException exception) {
+		log.error("系统异常", exception);
+		Map<String, Object> map = new HashMap<>();
+		map.put("mesage", exception.getMessage());
+		map.put("code", HttpStatus.BAD_REQUEST.value());
+		return map;
+	}
+
+	@ExceptionHandler({ MissingServletRequestParameterException.class, HttpMessageNotReadableException.class,
+			UnsatisfiedServletRequestParameterException.class, MethodArgumentTypeMismatchException.class })
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public Object badRequestException(Exception exception) {
+		log.error("系统异常", exception);
+		Map<String, Object> map = new HashMap<>();
+		map.put("mesage", exception.getMessage());
+		map.put("code", HttpStatus.BAD_REQUEST.value());
+		return map;
+	}
+
+	@ExceptionHandler(Exception.class)
+	public Object Exception(Exception throwable) {
+		log.error("系统异常", throwable);
+		Map<String, Object> map = new HashMap<>();
+		map.put("mesage", throwable.getMessage());
+		map.put("code", HttpStatus.INTERNAL_SERVER_ERROR.value());
 		return map;
 
 	}
