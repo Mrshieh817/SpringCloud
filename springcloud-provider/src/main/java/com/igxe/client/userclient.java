a7e15f975a007b903b87919260d3327fb20bc9cb
Mrshieh817 <18716398693@163.com>
2019-12-07 15:23:08 +0800

    测试

--- springcloud-provider/src/main/java/com/igxe/client/userclient.java
+++ springcloud-provider/src/main/java/com/igxe/client/userclient.java
@@ -17,7 +17,7 @@ import com.igxe.tool.FeignConfiguration;
 * 特别注明:@FeignClient 里面的名称是spring.applicatoin.name  名称 如果是多项目的话 名称要一样 参考 provider  和provider1
 *  @GetMapping("product/test") 这个地址是暴露的api地址 类似一个controller 里面的某个方法
 */
-@FeignClient(name="product", configuration = FeignConfiguration.class)
+@FeignClient(name="product", configuration = FeignConfiguration.class,fallback = UserClientFallback.class)
 public interface userclient {
 
 	/**
