839e01347d6176922bd09b0c055ca6f2a22648f8
XieChengFei <18716398693@163.com>
2021-06-18 11:24:03 +0800

    测试线程池和CountDownLatch

--- com.xcf.mybatis/src/main/java/com/xcf/mybatis/Controller/TestMain.java
+++ com.xcf.mybatis/src/main/java/com/xcf/mybatis/Controller/TestMain.java
@@ -2,6 +2,12 @@ package com.xcf.mybatis.Controller;
 
 import java.sql.Timestamp;
 import java.util.TimeZone;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /** 
 * @author xcf 
@@ -15,8 +21,31 @@ public class TestMain {
 		//long yesterday=System.currentTimeMillis()-24*60*60*1000;//昨天的这一时间的毫秒数
 		//System.out.println(new Timestamp(current));//当前时间
 		//System.out.println(new Timestamp(yesterday));//昨天这一时间点
-		System.out.println(new Timestamp(zero));//今天零点零分零秒
-		System.out.println(new Timestamp(twelve));//今天23点59分59秒
+		//System.out.println(new Timestamp(zero));//今天零点零分零秒
+		//System.out.println(new Timestamp(twelve));//今天23点59分59秒
+		//ThreadPoolExecutor executor=new ThreadPoolExecutor(5,10,10L,TimeUnit.SECONDS,new LinkedBlockingDeque<>(1000), new ThreadPoolExecutor.AbortPolicy());
+		ExecutorService executor = Executors.newSingleThreadExecutor();
+		CountDownLatch latch=new CountDownLatch(1);
+		executor.execute(()->{
+			System.out.println("传进来的是：A");
+			latch.countDown();
+		});
+		
+		executor.execute(()->{
+			System.out.println("传进来的是：B");
+			latch.countDown();
+		});
+		try {
+			latch.await();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	    //System.out.println(executor.getActiveCount());	
+	    //executor.isShutdown();
+	}
+	
+	public static void sendinfo(String in) {
+		System.out.println("传进来的是："+in);
 	}
 
 }
